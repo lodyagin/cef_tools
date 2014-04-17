7ea82e424f71a24f4e2c0ab65541714e23a8c085
Sergei Lodyagin <serg@kogorta.dp.ua>
2014-04-17 11:49:54 +0300

    xpath_test -> browser

--- tests/xpath_test.cpp
+++ tests/xpath_test.cpp
@@ -1,4 +1,7 @@
+#include <string.h>
+#include "include/cef_command_line.h"
 #include "xpath.h"
+#include "offscreen.h"
 #include "gtest/gtest.h"
 
 using namespace xpath;
@@ -13,5 +16,14 @@ TEST(XpathBasic, Wrap) {
 int main(int argc, char* argv[])
 {
   testing::InitGoogleTest(&argc, argv);
+
+  char** argv2 = new char*[argc+1];
+  int argc2;
+  for (argc2 = 0; argc2 < argc; argc2++)
+    argv2[argc2] = argv[argc2];
+  SCHECK(argv2[argc2++] = strdup("--off-screen"));
+
+  offscreen(argc2, argv2);
+
   return RUN_ALL_TESTS();
 }
